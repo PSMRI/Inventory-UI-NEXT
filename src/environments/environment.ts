c8ba37666629f977ad7687021af401c830540a9a
Nagendra Babu Kokkiligadda <NA20311191@wipro.com>
2024-03-29 17:51:50 +0530

    bugg fix CSS changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -24,20 +24,20 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
-const commonIP = 'http://183.82.107.186:8080/';
-const inventoryIP = 'http://183.82.107.186:8080/';
-const mmuIP = 'http://183.82.107.186:8080/';
-const FHIRIP = 'http://183.82.107.186:8080/';
+const commonIP = 'https://amritwprdev.piramalswasthya.org/';
+const inventoryIP = 'https://amritwprdev.piramalswasthya.org/';
+const mmuIP = 'https://amritwprdev.piramalswasthya.org/';
+const FHIRIP = 'https://amritwprdev.piramalswasthya.org/';
 // const COMMON_API_OPEN = `http://${IP}:8080/apiman-gateway/IEMR/Common/open/`;
 // const COMMON_API = `http://${IP}:8080/apiman-gateway/IEMR/Common/1.0/`;
 // const INVENTORY_API = `http://${IP}:8080/apiman-gateway/IEMR/Inventory/1.0/`;
 // const MMU_API = `http://${IP}:8080/apiman-gateway/IEMR/MMU/1.0/`;
 
-const COMMON_API_OPEN = `${commonIP}commonapi-v1.0/`;
-const COMMON_API = `${commonIP}commonapi-v1.0/`;
-const INVENTORY_API = `${inventoryIP}Inventoryapi-v1.0/`;
-const MMU_API = `${mmuIP}mmuapi-v1.0/`;
-const FHIR_API = `${FHIRIP}fhirapi-v1.0/`;
+const COMMON_API_OPEN = `${commonIP}commonapi-v0.1/`;
+const COMMON_API = `${commonIP}commonapi-v0.1/`;
+const INVENTORY_API = `${inventoryIP}Inventoryapi-v0.1/`;
+const MMU_API = `${mmuIP}mmuapi-v0.1/`;
+const FHIR_API = `${FHIRIP}fhirapi-v0.1/`;
 // const FHIR_API = `http://localhost:8080/fhirapi-v1.0/`;
 
 export const environment = {
